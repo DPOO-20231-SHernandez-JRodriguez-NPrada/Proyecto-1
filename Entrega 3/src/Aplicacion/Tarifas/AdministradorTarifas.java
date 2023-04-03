2f7f43cd7eeb712031ccfbf1b9986c0d09e22292
Samu2005Inge <sa.hernandezd1@uniandes.edu.co>
2023-04-03 13:47:03 -0500

    tarifa

--- Entrega 3/src/Aplicacion/Tarifas/AdministradorTarifas.java
+++ Entrega 3/src/Aplicacion/Tarifas/AdministradorTarifas.java
@@ -3,8 +3,14 @@ package Aplicacion.Tarifas;
 import java.util.ArrayList;
 
 public class AdministradorTarifas {
+    private ArrayList<Tarifa> tarifas;
 
     public void AsignarTarifas(ArrayList<Tarifa> datosTarifas) {
+        this.tarifas = datosTarifas;
+    }
+
+    public void cambiarTarifa(String tipo){
+
     }
     
 }
--- Entrega 3/src/Aplicacion/Tarifas/Tarifa.java
+++ Entrega 3/src/Aplicacion/Tarifas/Tarifa.java
@@ -1,5 +1,13 @@
 package Aplicacion.Tarifas;
 
 public class Tarifa {
+    private Double[] preciosEstandar;
+    private Double[] preciosSuite;
+    private Double[] preciosDoble;
+    private int anio;
+
+    public Tarifa(){
+
+    }
     
 }
