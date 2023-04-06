813c6e6f298279930fdccce645eb4b0f85aa9cd7
Gatoprominus <je.rodriguezd12@uniandes.edu.co>
2023-04-05 21:50:43 -0500

    Cambios proyecto

--- Entrega 3/src/Aplicacion/EnrutadorPrincipal.java
+++ Entrega 3/src/Aplicacion/EnrutadorPrincipal.java
@@ -3,6 +3,7 @@ package Aplicacion;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
 import Aplicacion.Habitaciones.AdministradorHabitaciones;
 import Aplicacion.Habitaciones.HabitacionBase;
 import Aplicacion.Huespedes.AdministradorHuespedes;
@@ -108,7 +109,64 @@ public class EnrutadorPrincipal {
     public String ComprobarLogin(String usuario, String contrasenia) {
         return adminLogin.ComprobarLogin(usuario, contrasenia);
     }
+ 
+    public void cambiarTarifa(String tipo, int anio, Double precio, String finicio, String ffinal, ArrayList<String> dias)
+    {
+        this.adminTarifas.cambiarTarifa(tipo, anio, precio, finicio, ffinal, dias);
+    }
 
 
-
+    public void CrearReserva(String documento, String estadoReserva, int personasEsperadas, String fechainicial, String fechafinal, ArrayList<HabitacionBase> HabitacionesBs, String nombre, String correo, int celular)
+    {
+        Huesped huesped = adminHuespedes.crearHuesped(nombre, documento, correo, celular);
+       adminReservas.CrearReserva(documento,estadoReserva,personasEsperadas,fechainicial,fechafinal,HabitacionesBs, huesped);
+    }
+    public String VerReserva(String documento)
+    {
+        String Texto = adminReservas.VerReserva(documento);
+        return Texto;
+    }
+    public void EliminarReserva(String documento, String fechactual)
+    {
+        adminReservas.eliminarReserva(documento,fechactual);
+    }
+    public void AñadirServicio(String documento, String Nombreservicio, String descripcion, String fecha, boolean pagado,double precio)
+    {
+        ServicioBase servicio = adminServicios.darServicio(Nombreservicio);
+        if(servicio.getRegistro().equals("habitacion"))
+        {
+            adminReservas.AñadirServicio(documento,servicio,descripcion,fecha,pagado,precio);
+        }
+        else
+        {
+            adminHuespedes.AñadirServicio(documento,servicio,descripcion,fecha,pagado,precio);
+        }
+    }
+    public double precioProducto(String nombre)
+    {
+        double precio = adminServicios.precioProducto(nombre);
+        return precio;
+    }
+    public void HacerCheckin()
+    {
+        
+    }
+    public void HacerCheckout()
+    {
+        
+    }
+    public ArrayList<HabitacionBase> buscarHabitaciones(Boolean cocina, Boolean balcon, Boolean vista, String fechai, String fechaf, String tipo)
+    {
+        ArrayList<HabitacionBase> HabitacionesBs = adminHabitaciones.buscarHabitaciones(cocina, balcon, vista, fechai, fechaf, tipo);
+        return HabitacionesBs;
+    }
+    public void modificarEstadoOcupado(String id,String fechainicial, String fechafinal,boolean cambio)
+    {
+        adminHabitaciones.modificarEstadoOcupado(id, fechainicial, fechafinal, cambio);
+    }
+    public Huesped crearHuesped(String nombre, String doc, String correo, int celular)
+    {
+        Huesped huesped = adminHuespedes.crearHuesped(nombre, doc, correo, celular);
+        return huesped;
+    }
 }
